b998e34a691143399b2d59d27116cb7e3d0bdb0a
Meirza <meirza.heyder@gmail.com>
2025-08-23 14:55:15 +0700

    add vscode keymaps

--- lua/vscode-config.lua
+++ lua/vscode-config.lua
@@ -16,6 +16,25 @@ vim.opt.timeoutlen = 300
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+local vs = require('vscode')
+
+-- File navigation
+vim.keymap.set('n', '<leader><leader>', function() vs.action('workbench.action.showAllEditors') end, { desc = 'Show Buffers' })
+vim.keymap.set('n', '<leader>sf', function() vs.action('find-it-faster.findFiles') end, { desc = 'Search Files' })
+vim.keymap.set('n', '<leader>sg', function() vs.action('find-it-faster.findWithinFiles') end, { desc = 'Search Grep' })
+vim.keymap.set('n', '<leader>sw', function() 
+  vim.cmd('normal! yiw') -- Yank the current word under cursor
+  vs.action('find-it-faster.findWithinFiles') -- Open find-it-faster and paste the word
+  vim.defer_fn(function() vs.action('workbench.action.terminal.paste') end, 100) -- Paste the word into the terminal
+end, { desc = 'Search current Word' })
 
 -- Note: Most plugins won't be loaded in VSCode mode
 -- Additional VSCode-specific configurations can be added here
